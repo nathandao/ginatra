a4d5b11f56a8615ca481ef670450a41aa0f9c024
Nathan Dao <nathan@exove.fi>
2015-05-25 23:24:13 +0300

    repo model commits & author init

--- lib/models.rb
+++ lib/models.rb
@@ -1,40 +1,43 @@
 module Ginatra
   class Repo
-    REPO_PATHS = [
-                  { :path => '/Users/nathan/rails_projects/antea', :name => 'An Tea' },
-                  { :path => '/Users/nathan/Sites/omegawave/omegawave', :name => 'Omegawave' }
-                ]
-
-    att_accessor :path, :authors, :commits
-
-    def initialize(path)
-
-      commits = []
-      log_params = "--pretty=full --full-history --shortstat"
-      log_format = '"commit=>%h, author=>%an, dade=>%ai<--ginatra-entry-->"'
-
-      commit_logs = `git -C #{path} log #{log_params} --format=#{log_format}`.split("<--ginatra-entry-->")
-
-      commit_logs.each do |commit_log|
-        c_meta = {}
-
-        commit_log.split("\n").each do |commit_part|
-          next unless !commit_part.empty?
-
-          if (commit_part.index('commit=>') == 0)
-            commit_part.split(/, /).inject(Hash.new{ |h, k| h[k] = nil }) do |h, s|
-              k,v = s.split(/=>/)
-              h[k] = v
-              c_meta = [c_meta, h].inject(&:merge)
-            end
-          end
-        end
-        commits << c_meta
-      end
-
-    end
+    attr_accessor :path, :authors, :commits
 
     class << self
+    end
+
+
+    def initialize(path)
+      @path = path
+      @commits = Array.new()
+      @authors = Array.new()
+
+      r_path = "~/Sites/vagrant-nesteoil/git/nesteoil"
+      l_params = "--pretty=full --full-history --shortstat"
+      l_format = '"commit=>%h, author=>%an, date=>%ai<--c-entry-->"'
+      l_command = "git -C #{r_path} log #{l_params} --format=#{l_format}"
+
+      c_logs = `#{l_command}`.split("<--c-entry-->")
+
+      c_logs.each do |c_log|
+        c_meta = Hash.new()
+
+        c_log.split("\n").reject { |c|
+          c.empty? }.each do |c_raw|
+          if c_raw.index('commit=>') == 0
+            c_raw.split(/, /).inject(Hash.new{ |h, k| h[k] = nil }) do |h, s|
+              k,v = s.split(/=>/)
+              @authors = @authors | [v] unless k.to_s != "author"
+              h[k.to_sym] = v
+              c_meta = [c_meta, h].inject(&:merge)
+            end
+          else
+            # file & line changes
+            c_raw.split(/, /).map{ |s|
+              s.match(/[0-9]/).to_s.to_i }
+          end
+        end
+        @commits << c_meta
+      end
     end
   end
 end
