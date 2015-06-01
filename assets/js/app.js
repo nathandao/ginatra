5a85341ebfb908148495d65a64a417c15f600e08
Nathan Dao <nathan@exove.fi>
2015-06-01 15:28:36 +0300

    organize assets

---
+++ assets/js/app.js
@@ -0,0 +1,6 @@
+var $ = require('jquery');
+var LineChart = require("react-chartjs").Line;
+var BarChart = require("react-chartjs").Bar;
+var RadarChart = require("react-chartjs").Radar;
+var PolarAreaChart = require("react-chartjs").PolarArea;
+var DoughnutChart = require("react-chartjs").Doughnut;
