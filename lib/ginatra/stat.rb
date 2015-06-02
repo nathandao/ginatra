62c86a9b810542fdb1cb067fa2195707f45ec9bd
Nathan Dao <nathan@exove.fi>
2015-06-03 02:14:04 +0300

    reformat lines to hash with repo key

--- lib/ginatra/stat.rb
+++ lib/ginatra/stat.rb
@@ -1,21 +1,12 @@
 module Ginatra
   class Stat
     class << self
-      def commits(repo_id)
+      def commits repo_id
         get_repo(repo_id).commits
       end
 
-      def authors(repo_id)
-        get_repo(repo_id).commits.group_by { |commit|
-          commit.first[1]['author']
-        }.map { |name, commits|
-          {
-           'name' => name,
-           'commits' => commits.size,
-           'additions' => get_additions(commits),
-           'deletions' => get_deletions(commits)
-          }
-        }
+      def commits_between repo_id, date_range = []
+        get_repo(repo_id).commits_between date_range
       end
 
       def all_commits
@@ -29,6 +20,19 @@ module Ginatra
           params = repo[1].merge({'id' => id})
           output[id] = Repository.new(params).commits_between date_range
           output
+        }
+      end
+
+      def authors repo_id
+        get_repo(repo_id).commits.group_by { |commit|
+          commit.first[1]['author']
+        }.map { |name, commits|
+          {
+           'name' => name,
+           'commits' => commits.size,
+           'additions' => get_additions(commits),
+           'deletions' => get_deletions(commits)
+          }
         }
       end
 
@@ -54,23 +58,23 @@ module Ginatra
 
       def all_lines_between date_range = []
         repos = Ginatra::Config.repositories
-        repos.inject(0) { |line_count, repo|
+        repos.inject(Hash.new) { |lines, repo|
           id = repo[0]
-          line_count += lines_between id, date_range
-          line_count
+          lines[id] = lines_between id, date_range
+          lines
         }
       end
 
       private
 
-      def get_repo(repo_id)
+      def get_repo repo_id
         repo = Ginatra::Config.repositories[repo_id]
         return nil if repo.nil?
         repo['id'] = repo_id
-        Ginatra::Repository.new(repo)
+        Ginatra::Repository.new repo
       end
 
-      def get_commit_value(commits, key)
+      def get_commit_value commits, key
         value = 0
         commits.each do |commit|
           commit.each do |k, v|
@@ -82,12 +86,12 @@ module Ginatra
         value
       end
 
-      def get_additions(commits)
-        get_commit_value(commits, 'additions')
+      def get_additions commits
+        get_commit_value commits, 'additions'
       end
 
-      def get_deletions(commits)
-        get_commit_value(commits, 'deletions')
+      def get_deletionsx commits
+        get_commit_value commits, 'deletions'
       end
     end
   end
