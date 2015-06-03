045f3059663067c130c72fa264d37d5a72e7c54d
Nathan Dao <nathan@exove.fi>
2015-06-03 08:25:07 +0300

    data for line charts

--- lib/ginatra/chart.rb
+++ lib/ginatra/chart.rb
@@ -19,6 +19,45 @@ module Ginatra
         }
       end
 
+      def line_chart_all_commits date_range = []
+        line_chart line_chart_all_commits_data date_range
+      end
+
+      def line_chart_all_commits_data date_range = []
+        init_data = {'labels' => [], 'datasets' => [{}]}
+        all_commits_between(date_range).inject(init_data) { |result, repo|
+          repo_id = repo[0]
+          commits = repo[1]
+          p repo_id
+          result['labels'] << repo_id
+          result['datasets'][0]['label'] ||= "Commits"
+          result['datasets'][0]['data'] ||= []
+          result['datasets'][0]['data'] += [commits.size]
+          result
+        }
+      end
+
+      def line_chart_all_lines date_range = []
+        line_chart line_chart_all_lines_data date_range
+      end
+
+      def line_chart_all_lines_data date_range = []
+        init_data = {'labels' => [], 'datasets' => [{}]}
+        all_lines_between(date_range).inject(init_data) { |result, repo|
+          repo_id = repo[0]
+          lines_count = repo[1]
+          result['labels'] << repo_id
+          result['datasets'][0]['label'] ||= "Lines"
+          result['datasets'][0]['data'] ||= []
+          result['datasets'][0]['data'] << lines_count
+          result
+        }
+      end
+
+      def line_chart_combined_all_lines_commits date_range = []
+        line_chart line_chart_combine_data line_chart_all_lines_data, line_chart_all_commits_data
+      end
+
       private
 
       def round_chart data = Hash.new
@@ -36,6 +75,40 @@ module Ginatra
         }
       end
 
+      def line_chart data = Hash.new
+        c = colors
+        init_data = {'labels' => [], 'datasets' => []}
+        data.inject(init_data) { |output, v|
+          key = v[0]
+          if key == 'labels'
+            output[key] = v[1]
+          else
+            output[key] = v[1].inject([]) { |datasets, dataset|
+              color = c.pop
+              datasets << dataset.merge({'fillColor' => rgba(color, 0.2),
+                                         'strokeColor' => rgba(color),
+                                         'pointColor' => rgba(color),
+                                         'pointStrokeColor' => '#fff',
+                                         'pointHighlightFill' => '#fff',
+                                         'pointHighlightStroke' => rgba(color)})
+              c = colors if c.empty?
+              datasets
+            }
+          end
+          output
+        }
+      end
+
+      def line_chart_combine_data data1, data2
+        if data1['labels'] == data2['labels']
+          return {'labels' => data1['labels'],
+                  'datasets' => data1['datasets'] + data2['datasets']
+                 }
+        else
+          return false
+        end
+      end
+
       def colors
         ['#114b5f','#028090','#e4fde1','#456990','#f45b69']
       end
@@ -44,6 +117,13 @@ module Ginatra
         ['#114b5f','#028090','#e4fde1','#456990','#f45b69']
       end
 
+      def rgba hex, a = 1
+        hex += hex[1..-1] if hex.length == 4
+        hex.match(/#(..)(..)(..)/).to_a[1..-1].inject('rgba(') { |rgba, v|
+          rgba += "#{v.hex},"
+          rgba
+        } + "#{a})"
+      end
     end
 
   end
