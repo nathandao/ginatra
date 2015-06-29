feddadfa0b60d63007f896d03787197e0584f59b
Nathan Dao <nathan@exove.fi>
2015-06-29 13:09:26 +0200

    update new relative path to assets and data folder

--- lib/ginatra.rb
+++ lib/ginatra.rb
@@ -20,8 +20,8 @@ module Ginatra
     register Sinatra::AssetPack
 
     set :views, File.expand_path('../views', File.dirname(__FILE__))
-    set :root, Ginatra::Env.root || ::File.expand_path('../../', ::File.dirname(__FILE__))
-    set :data, Ginatra::Env.data || ::File.expand_path('../../data/', ::File.dirname(__FILE__))
+    set :root, Ginatra::Env.root || ::File.expand_path('../', ::File.dirname(__FILE__))
+    set :data, Ginatra::Env.data || ::File.expand_path('../data/', ::File.dirname(__FILE__))
 
     assets do
       # Custom assets mangement
