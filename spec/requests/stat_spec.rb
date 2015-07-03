0f9b0b102937d51aea7a33c902af755dbdf159b5
Nathan Dao <nathan@exove.fi>
2015-07-03 10:33:39 +0300

    refactor commits stat rspec

--- spec/requests/stat_spec.rb
+++ spec/requests/stat_spec.rb
@@ -1,46 +1,26 @@
 require_relative '../spec_helper.rb'
 
 describe Ginatra::Stat do
+  before { @stat = Ginatra::Stat }
+
   describe "commits" do
-    before(:each) do
-      @stat = Ginatra::Stat
-      @repo_1 = get_repo('repo_1')
-      @repo_2 = get_repo('repo_2')
-    end
+    full_params = {from: "9 Feb 2015",
+                   til: "9 March 2015",
+                   by: "Nathan Dao",
+                   in: "repo_1"}
 
-    context "without custom params" do
-      let(:commits) { @stat.commits }
+    # Test all combinations of params
+    (0..4).to_a.each do |size|
+      full_params.keys.combination(size).to_a.each do |combi|
+        params = combi.inject({}) { |p, k|
+          p.merge!({k => full_params[k]})
+          p
+        }
 
-      it "should include all repos" do
-        expect(commits.keys[0]).to eq @repo_1.id
-        expect(commits.keys[1]).to eq @repo_2.id
-      end
-
-      it "should have all commits data" do
-        expect(same_commit_arrays?(commits.values[0], @repo_1.commits)).to be true
-        expect(same_commit_arrays?(commits.values[1], @repo_2.commits)).to be true
-      end
-    end
-
-    context "with valid custom params" do
-      full_params = {from: "9 Feb 2015",
-                     til: "9 March 2015",
-                     by: "Nathan Dao",
-                     in: "repo_1"}
-
-      # Test all combinations of params
-      (1..4).to_a.each do |s|
-        full_params.keys.combination(s).to_a.each do |combi|
-          params = combi.inject({}) { |p, k|
-            p.merge!({k => full_params[k]})
-            p
-          }
-
-          context "#{params.to_s}" do
-            it "should return return the correct commits data" do
-              Ginatra::Stat.commits(params).each do |v|
-                expect(same_commit_arrays?(v[1], get_repo(v[0]).commits(params))).to be true
-              end
+        context "#{params.to_s}" do
+          it "should return return the correct commits data" do
+            Ginatra::Stat.commits(params).each do |v|
+              expect(same_commit_arrays?(v[1], get_repo(v[0]).commits(params))).to be true
             end
           end
         end
@@ -57,5 +37,9 @@ describe Ginatra::Stat do
         end
       end
     end
+  end
+
+  describe "commits_overview" do
+
   end
 end
