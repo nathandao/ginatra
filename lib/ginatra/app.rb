dd5c7c2a2f866efe12b19a196bd740e5a22c05b3
Nathan Dao <nathan@exove.fi>
2015-10-18 21:01:53 +0300

    add missing required gems

--- lib/ginatra/app.rb
+++ lib/ginatra/app.rb
@@ -1,27 +1,30 @@
 require 'em-websocket'
 require 'eventmachine'
-require 'rufus-scheduler'
+require 'unicorn'
+require 'rainbows'
 
 Encoding.default_external = 'utf-8' if RUBY_VERSION =~ /^2.0/
 
 module Ginatra
-  EM.run {
+  class Ginatra
+    EM.run {
 
-    # Sinatra App
-    class App < Ginatra::API
-    end
+      # Sinatra App
+      class App < Ginatra::API
+      end
 
-    # websocket server
-    Ginatra::WebsocketServer.start
+      # websocket server
+      Ginatra::WebsocketServer.start
 
-    # start rainbows
-    port = Ginatra::Env.port || 8080
-    rackup = Unicorn::Configurator::RACKUP
-    rackup[:set_listener] = true
-    rackup[:port] = port
-    options = rackup[:options]
-    options[:config_file] = File.expand_path('./rainbows.conf', Ginatra::API.root)
+      # start rainbows
+      port = Ginatra::Env.port || 8080
+      rackup = Unicorn::Configurator::RACKUP
+      rackup[:set_listener] = true
+      rackup[:port] = port
+      options = rackup[:options]
+      options[:config_file] = File.expand_path('./rainbows.conf', Ginatra::API.root)
 
-    Rainbows::HttpServer.new(Ginatra::App, options).start
-  }
+      Rainbows::HttpServer.new(Ginatra::App, options).start
+    }
+  end
 end
