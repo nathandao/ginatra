2a530efb8840e714bd278847e350e337bf4f7989
nathandao <nathan@guynathan.com>
2016-01-15 13:03:56 +0200

    add overview stat

--- lib/ginatra/core/stat.rb
+++ lib/ginatra/core/stat.rb
@@ -7,33 +7,8 @@ module Ginatra
         Ginatra::Helper.query_commits(params)
       end
 
-      # def commits(params = {})
-      #   if params[:in].nil?
-      #     repos = Ginatra::Config.repositories
-      #     repos.inject({}) { |output, repo|
-      #       repo_id = repo[0]
-      #       output[repo_id] = Ginatra::Helper.get_repo(repo_id).commits(params)
-      #       output
-      #     }
-      #   else
-      #     { params[:in] => Ginatra::Helper.get_repo(params[:in]).commits(params) }
-      #   end
-      # end
-
-      def commits_count(params = {})
-        commits_count = nil
-        if params[:in].nil?
-          repos = Ginatra::Config.repositories
-          commits_count = repos.inject(0) { |count, repo|
-            repo_id = repo[0]
-            count += Ginatra::Helper.get_repo(repo_id).commits(params).size
-            count
-          }
-        else
-          repo_id = params[:in]
-          commits_count = Ginatra::Helper.get_repo(repo_id).commits(params).size
-        end
-        return commits_count.nil? ? 0 : commits_count
+      def overview(params = {})
+        Ginatra::Helper.query_overview(params)
       end
 
       def repo_overview(params = {})
@@ -46,33 +21,33 @@ module Ginatra
         }
       end
 
-      def commits_overview(params = {})
-        commits_count = 0
-        additions = 0
-        deletions = 0
-        lines = 0
-        last_commit = Time.new
-        first_commit = Time.new
-        last_commit_info = {}
-        commits(params).each do |repo_id, repo_commits|
-          unless repo_commits.nil?
-            commits_count += repo_commits.size
-            additions += Ginatra::Helper.get_additions(repo_commits)
-            deletions += Ginatra::Helper.get_deletions(repo_commits)
-            lines += additions - deletions
-            unless repo_commits[0].nil?
-              last_commit = repo_commits[0].flatten[1]['date']
-              first_commit = repo_commits[-1].flatten[1]['date']
-              last_commit_info = repo_commits[0].flatten[1]
-            end
-          end
-        end
-        { commits_count: commits_count, additions: additions,
-          deletions: deletions, lines: lines,
-          last_commit: Chronic.parse(last_commit).to_f * 1000,
-          first_commit: Chronic.parse(first_commit).to_f * 1000,
-          last_commit_info: last_commit_info }
-      end
+      # def commits_overview(params = {})
+      #   commits_count = 0
+      #   additions = 0
+      #   deletions = 0
+      #   lines = 0
+      #   last_commit = Time.new
+      #   first_commit = Time.new
+      #   last_commit_info = {}
+      #   commits(params).each do |repo_id, repo_commits|
+      #     unless repo_commits.nil?
+      #       commits_count += repo_commits.size
+      #       additions += Ginatra::Helper.get_additions(repo_commits)
+      #       deletions += Ginatra::Helper.get_deletions(repo_commits)
+      #       lines += additions - deletions
+      #       unless repo_commits[0].nil?
+      #         last_commit = repo_commits[0].flatten[1]['date']
+      #         first_commit = repo_commits[-1].flatten[1]['date']
+      #         last_commit_info = repo_commits[0].flatten[1]
+      #       end
+      #     end
+      #   end
+      #   { commits_count: commits_count, additions: additions,
+      #     deletions: deletions, lines: lines,
+      #     last_commit: Chronic.parse(last_commit).to_f * 1000,
+      #     first_commit: Chronic.parse(first_commit).to_f * 1000,
+      #     last_commit_info: last_commit_info }
+      # end
 
       def authors(params = {})
         if params[:in].nil?
